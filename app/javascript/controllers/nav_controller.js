6f7c748bc5d72c8db908566afd33a212e0db0482
lucas vittaz <lucasvittaz.pro@gmail.com>
2022-07-14 18:08:07 +0200

    nav + banner + works

--- app/javascript/controllers/nav_controller.js
+++ app/javascript/controllers/nav_controller.js
@@ -7,16 +7,31 @@ export default class extends Controller {
 
       for (let i = 0; i < btns.length; i++  ){
 
-        btns[i].addEventListener('click', (e)=>{
-          btns.forEach(btn => {
-            console.log(btn)
-            btn.classList.remove('active')
-          });
-          console.log(e.target)
+        btns[i].addEventListener('click', (e) =>{
+          for (let i = 0; i < btns.length; i++  ){
+          btns[i].classList.remove('active')
+          }
           e.target.classList.add('active')
         });
 
       }
 
+    const h1 = document.querySelector('h1')
+    const h2 = document.querySelector('h2')
+    function typingEffect(elt, speed) {
+      let text = elt.innerHTML
+      elt.innerHTML= ""
+      let i = 0
+      let timer = setInterval(() => {
+        if (i < text.length){
+          elt.append(text.charAt(i))
+          i++
+        } else {
+          clearInterval(timer)
+        }
+      }, speed);
+    }
+    typingEffect(h1, 100)
+    typingEffect(h2, 150)
   }
 }
