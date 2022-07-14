8ce0b90dc1375f48e0413b4e553ab966c035049e
lucas vittaz <lucasvittaz.pro@gmail.com>
2022-07-14 16:52:54 +0200

    adding javascript to handle carousel controls

--- app/javascript/controllers/slider_controller.js
+++ app/javascript/controllers/slider_controller.js
@@ -1,39 +1,50 @@
 import { Controller } from "@hotwired/stimulus"
 
 export default class extends Controller {
-
   connect() {
-      const items = document.querySelectorAll('.img-slider')
-      const slideNumber = items.length
+    const items = document.querySelectorAll('.img-slider');
+    const slideNumber = items.length;
+    const text = document.querySelectorAll('.cont-text');
+    const controls = document.querySelectorAll('.carousel-control');
+    const next = document.querySelector('.btn-nav');
+    let count = 0;
 
-      const text = document.querySelectorAll('.cont-text')
-      const textNumber = text.length
+    function nextSlide(){
+      items[count].classList.remove('active');
+      text[count].classList.remove('active');
+      controls[count].classList.remove('active');
+      if (count < slideNumber -1) {
+        count += 1;
+      } else {
+        count = 0;
+      }
+      items[count].classList.add('active');
+      text[count].classList.add('active');
+      controls[count].classList.add('active');
+    }
+    next.addEventListener('click', nextSlide);
 
-      const next = document.querySelector('.btn-nav')
+    for (let i = 0; i < controls.length; i++){
+      controls[i].addEventListener('click', function(e) {
 
-      let count = 0
-
-      function nextSlide() {
-        items[count].classList.remove('active')
-        text[count].classList.remove('active')
-        if (count < slideNumber -1) {
-          count += 1
-        } else {
-          count = 0;
-        }
-        console.log(count)
-        items[count].classList.add('active')
-        text[count].classList.add('active')
+      count = i;
+      for (let i = 0; i < controls.length; i++){
+        items[i].classList.remove('active');
+        text[i].classList.remove('active');
+        controls[i].classList.remove('active');
       }
 
-      next.addEventListener('click', nextSlide)
+      items[i].classList.add('active');
+      text[i].classList.add('active');
+      controls[i].classList.add('active');
+      });
+    }
 
-      function keyPress(e) {
-        if (e.keyCode === 39) {
-          nextSlide()
-        }
+    function keyPress(e) {
+      if (e.keyCode === 39) {
+        nextSlide();
       }
-      document.addEventListener('keydown', keyPress)
-
+    }
+    document.addEventListener('keydown', keyPress);
   }
 }
