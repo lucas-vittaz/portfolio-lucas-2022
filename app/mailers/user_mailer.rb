0121f9b1f1eabd5433342622a82ea11e2b047c2c
lucas vittaz <lucasvittaz.pro@gmail.com>
2022-07-20 14:12:39 +0200

    contact form

--- app/mailers/user_mailer.rb
+++ app/mailers/user_mailer.rb
@@ -1,9 +1,9 @@
-class ContactMailer < ApplicationMailer
+class UserMailer < ApplicationMailer
   def contact(user_name, user_email, user_message)
     @user_name = user_name
     @user_email = user_email
     @user_message = user_message
 
-    mail(to: user_name, subject: "Nouveau message")
+    mail(to: 'lucasvittaz.pro@gmail.com', subject: "Nouveau message")
   end
 end
